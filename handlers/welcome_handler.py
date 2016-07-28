491e6df90446b4adada582f7a46866e34173363a
harikrish narasiman <Harikrishnan_Narasimhan@cable.comcast.com>
2016-07-28 19:41:41 -0400

    styling welcome page

--- handlers/welcome_handler.py
+++ handlers/welcome_handler.py
@@ -7,6 +7,6 @@ class WelcomeHandler(Handler):
         user_id = self.request.cookies.get("user_id")
         if user_id and make_secure_value(user_id.split('|')[0]) == user_id:
             username = User.get_by_id(int(user_id.split('|')[0])).username
-            self.write("<h2>Welcome to the blog, %s</h2>" % (username))
+            self.render("welcome.html",name = username)
         else:
             self.redirect("/blog/signup")
