c24ba8765a1f90168fe5d4153c404ed00cd4b05f
harikrish narasiman <Harikrishnan_Narasimhan@cable.comcast.com>
2016-07-28 22:51:08 -0400

    comment and likes

--- handlers/blog_list_handler.py
+++ handlers/blog_list_handler.py
@@ -1,8 +1,20 @@
 from handler import Handler
 from google.appengine.ext import db
+from models.post import Post
 
 class BlogListHandler(Handler):
     def get(self):
         posts = db.GqlQuery("SELECT * FROM Post"
                         " ORDER BY created DESC")
         self.render("index.html", posts = posts)
+
+    def post(self):
+        post_id = self.request.get("like")
+        post = Post.get_by_id(int(post_id))
+        if post.likes:
+            post.likes = post.likes + 1
+        else:
+            post.likes = 1
+        post.put()
+        print "====>",post.likes
+        self.redirect("/blog")
--- models/post.py
+++ models/post.py
@@ -3,4 +3,5 @@ from google.appengine.ext import db
 class Post(db.Model):
     title = db.StringProperty(required = True)
     content = db.TextProperty(required = True)
+    likes = db.IntegerProperty()
     created = db.DateTimeProperty(auto_now_add = True)
