9ea2350ccdfdc117fdac592983b350016c1f59b1
harikrish narasiman <Harikrishnan_Narasimhan@cable.comcast.com>
2016-07-31 00:39:27 -0400

    Comment like implementation

--- handlers/new_post_handler.py
+++ handlers/new_post_handler.py
@@ -2,21 +2,26 @@ from models.post import Post
 from handler import Handler
 
 class NewPostHandler(Handler):
-    def get(self):
-        if self.authenticated():
-            self.render("new.html")
+
+    def check_authenticated(self):
+        if not self.authenticated():
+            self.login_redirect()
         else:
-            self.redirect("/blog/login")
-            alert_message = "Please login to access this feature"
+            return True
+
+    def get(self):
+        if self.check_authenticated():
+            self.render("new.html")
 
     def post(self):
         title = self.request.get("subject")
         content = self.request.get("content")
 
-        if title and content:
-            a = Post(title = title, content = content)
-            a.put()
-            self.redirect("/blog/%s" % (a.key().id()))
-        else:
-            error = "Both title and art needed for submitting !"
-            self.render("new.html",subject = title, content = content, error = error)
+        if self.check_authenticated():
+            if title and content:
+                a = Post(title = title, content = content, author = self.user)
+                a.put()
+                self.redirect("/blog/%s" % (a.key().id()))
+            else:
+                error = "Both title and art needed for submitting !"
+                self.render("new.html",subject = title, content = content, error = error)
